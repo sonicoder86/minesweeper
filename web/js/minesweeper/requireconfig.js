c793b2fd895a59b3ceb0b14b3ed89218b1217325
blacksonic <soos.gabor86@gmail.com>
2013-08-12 21:45:34 +0200

    initial project layout

---
+++ web/js/minesweeper/requireconfig.js
@@ -0,0 +1,24 @@
+require.config({
+    baseUrl: 'js',
+    paths: {
+        jquery: 'lib/jquery/jquery-2.0.3',
+        underscore: 'lib/underscore-1.5.1',
+        backbone: 'lib/backbone/backbone-1.0.0',
+        marionette: 'lib/backbone/backbone.marionette-1.0.4',
+        text: 'lib/require/require.text-2.0.10'
+    },
+
+    shim: {
+        'jquery': {
+            exports: '$'
+        },
+        'backbone': {
+            deps: ['underscore', 'jquery'],
+            exports: 'Backbone'
+        },
+        'marionette': {
+            deps: ['backbone'],
+            exports: 'Backbone.Marionette'
+        }
+    }
+});
