fb125ab89a4dfee0339732e53c6dfdfb4abb4c05
blacksonic <soos.gabor86@gmail.com>
2013-08-25 22:39:30 +0200

    requirejs integration with jasmine node

--- tests/lib/requireconfig/node.js
+++ tests/lib/requireconfig/node.js
@@ -11,68 +11,15 @@
  * See requirejs-runner source for full invocation details.
  */
 var define,
-    requirejsOrig = require('requirejs'),
-    ostring = Object.prototype.toString,
-    path = require('path'),
-    isArray = function(it){
-        return ostring.call(it) === '[object Array]';
-    },
-    isFunction = function(it){
-        return ostring.call(it) === '[object Function]';
-    },
-    requirejs = function(deps, callback){
-        var retVal;
+    requirejs = require('requirejs'),
+    fs   = require('fs');
 
-        if(!isArray(deps) && typeof deps !== 'string'){
-            if(isArray(callback)){
-                retVal = requirejsOrig(deps, callback, arguments[2]);
-            } else {
-                retVal = requirejsOrig(deps, [], callback);
-            }
-        } else {
-            retVal = requirejsOrig(deps, callback);
-        }
+eval(fs.readFileSync(process.cwd()+'/web/js/minesweeper/requireconfig.js')+'');
 
-        return retVal;
-    };
-
-requirejsOrig.config({
-    //baseUrl: baseUrl,
-    nodeRequire: require,
-    paths: {
-        cs: 'cs'
-    }
+requirejs.config({
+    baseUrl: process.cwd()+'/web/js',
+    urlArgs: null
 });
-
-for(var key in requirejsOrig) {
-    requirejs[key] = requirejsOrig[key];
-}
-
-requirejs.config = function(config){
-    var alteredConfig = {};
-
-    for(var key in config) {
-        alteredConfig[key] = config[key];
-    }
-
-    if(alteredConfig.baseUrl){
-        var base = baseUrl.replace(/\\/g, '/'),
-            splitUrl = alteredConfig.baseUrl.replace(/\\/g, '/').split('/'),
-            index = 0;
-
-        for(; index < splitUrl.length; index++){
-            if(splitUrl[index] === '..'){
-                base = path.dirname(base);
-            } else {
-                base += '/' + splitUrl[index];
-            }
-        }
-
-        alteredConfig.baseUrl = base;
-    }
-
-    return requirejsOrig.config(alteredConfig);
-};
 
 require = requirejs;
 define = requirejs.define;
