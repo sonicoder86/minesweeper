8bc73248b6ea28b472a2a4d0a974e954bde06838
blacksonic <soos.gabor86@gmail.com>
2013-08-29 08:24:05 +0200

    maze spec

--- tests/minesweeper/model/mazespec.js
+++ tests/minesweeper/model/mazespec.js
@@ -7,11 +7,16 @@ define(['minesweeper/model/maze'], function(MazeModel) {
         });
 
         it('should have 9 fields with size 3', function() {
+            maze = new MazeModel({size: 3});
 
+            expect(maze.getFields().length).toEqual(9);
         });
 
-        it('should return same fields for consecutive calls', function() {
+        it('should have 1 bomb field when given 1 bombs', function() {
+            maze = new MazeModel({size: 3, bombs: 1});
+            var fieldsWithBomb = maze.getFields().where({isBomb: true});
 
+            expect(fieldsWithBomb.length).toEqual(1);
         });
     });
 });
