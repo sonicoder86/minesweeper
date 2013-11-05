64d8039d3b71186f17ed25bafe6fd3dda5940843
blacksonic <soos.gabor86@gmail.com>
2013-11-05 22:35:58 +0100

    mocha tests for nodejs requirejs setup

--- tests/server/requiresetup.spec.js
+++ tests/server/requiresetup.spec.js
@@ -1,7 +1,13 @@
-var assert = require('assert');
+var expect = require('chai').expect
+    requirejs = require('../../server/requiresetup');
 
-describe('requirejs', function() {
-    it('should exist', function() {
+describe('requirejssetup', function() {
+    it('should requirejs as function defined', function() {
+        expect(requirejs.requirejs).to.be.a('function');
+    });
 
+    it('should require given dependency', function() {
+        var maze = requirejs.requirejs('minesweeper/model/maze');
+        expect(maze).to.be.a('function');
     });
 });
