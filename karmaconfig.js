d0f1212e84299dadec9184a1bd60ae83cf462c97
blacksonic <soos.gabor86@gmail.com>
2014-01-12 11:58:19 +0100

    fix dependencies

--- karmaconfig.js
+++ karmaconfig.js
@@ -1,71 +1,73 @@
-module.exports = function(config) {
-  config.set({
+module.exports = function (config) {
+    "use strict";
+    config.set({
 
-    // base path, that will be used to resolve files and exclude
-    basePath: '',
+        // base path, that will be used to resolve files and exclude
+        basePath: '',
 
-    // frameworks to use
-    frameworks: ['jasmine', 'requirejs'],
-
-
-    // list of files / patterns to load in the browser
-    files: [
-        'web/js/minesweeper/requireconfig.js',
-        {pattern: 'web/js/**/*', included: false},
-        {pattern: 'tests/minesweeper/**/*.js', included: false},
-        'tests/lib/requireconfig/karma.js',
-        'tests/helper/jquery.js'
-    ],
-
-    preprocessors: {
-        '**/*.html': []
-    },
-
-    // list of files to exclude
-    exclude: [
-
-    ],
+        // frameworks to use
+        frameworks: ['jasmine', 'requirejs'],
 
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-    reporters: ['dots'],
+        // list of files / patterns to load in the browser
+        files: [
+            'web/js/minesweeper/requireconfig.js',
+            {pattern: 'web/js/**/*', included: false},
+            {pattern: 'tests/minesweeper/**/*.js', included: false},
+            'tests/lib/requireconfig/karma.js',
+            'tests/helper/jquery.js'
+        ],
+
+        preprocessors: {
+            '**/*.html': []
+        },
+
+        // list of files to exclude
+        exclude: [
+
+        ],
 
 
-    // web server port
-    port: 9876,
+        // test results reporter to use
+        // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+        reporters: ['dots'],
 
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+        // web server port
+        port: 9876,
 
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
+        // level of logging
+        // possible values:
+        // config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
 
-    // Start these browsers, currently available:
-    // - Chrome
-    // - ChromeCanary
-    // - Firefox
-    // - Opera
-    // - Safari (only Mac)
-    // - PhantomJS
-    // - IE (only Windows)
-    browsers: ['PhantomJS'],
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: false,
 
 
-    // If browser does not capture in given timeout [ms], kill it
-    captureTimeout: 6000,
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera
+        // - Safari (only Mac)
+        // - PhantomJS
+        // - IE (only Windows)
+        browsers: ['PhantomJS'],
 
 
-    // Continuous Integration mode
-    // if true, it capture browsers, run tests and exit
-    singleRun: false
-  });
+        // If browser does not capture in given timeout [ms], kill it
+        captureTimeout: 6000,
+
+
+        // Continuous Integration mode
+        // if true, it capture browsers, run tests and exit
+        singleRun: false
+    });
 };
