674ba43e5984457c04cc3bb88de98296f376c7ff
blacksonic <soos.gabor86@gmail.com>
2014-04-13 10:08:23 +0200

    add fav icon

--- Gruntfile.js
+++ Gruntfile.js
@@ -86,4 +86,5 @@ module.exports = function(grunt) {
 
     // Default task.
     grunt.registerTask('default', ['test']);
+    grunt.registerTask('minify', ['requirejs', 'cssmin']);
 };
--- server/app.js
+++ server/app.js
@@ -14,13 +14,10 @@ app.configure(function(){
     app.set('port', process.env.PORT || 3000);
     app.set('views', __dirname + '/views');
     app.set('view engine', 'ejs');
-    app.use(express.favicon());
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
+    app.use(express.favicon(__dirname + '/../web/favicon.ico'));
     app.use(app.router);
     app.use(express.static(path.join(__dirname, '../web')));
     app.use(express.static(path.join(__dirname, '../tests')));
-    app.use(express.logger('dev'));
     app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
