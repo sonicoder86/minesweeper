9a76390be6fa2d08431097d7813f890caa7de2e6
blacksonic <soos.gabor86@gmail.com>
2014-04-30 11:04:19 +0200

    fix requirejs config

--- web/js/minesweeper/requireconfig.js
+++ web/js/minesweeper/requireconfig.js
@@ -6,7 +6,7 @@ requirejs.config({
         underscore: 'lib/lodash/dist/lodash',
         backbone: 'lib/backbone/backbone',
         marionette: 'lib/backbone.marionette/lib/core/amd/backbone.marionette',
-        "backbone.wreqr": 'lib/backbone.wreqr/lib/amd/backbone.wreqr',
+        "backbone.wreqr": 'lib/backbone.wreqr/lib/backbone.wreqr',
         "backbone.babysitter": 'lib/backbone.babysitter/lib/backbone.babysitter',
         text: 'lib/requirejs-text/text',
         bootstrap: 'lib/bootstrap/dist/js/bootstrap',
